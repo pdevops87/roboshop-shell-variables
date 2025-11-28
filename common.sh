f74d996ee30d0ddce757aef3edf086a80d6be70b
Your Name <vemasani.pavani@gmail.com>
2025-11-28 12:23:59 +0530

    user status store in a variable

--- common.sh
+++ common.sh
@@ -14,8 +14,8 @@ dnf install nodejs -y &>>/tmp/roboshop.log
 echo -e "\\e[32m add user roboshop \\e[0m"
 echo
 id roboshop &>>/tmp/roboshop.log
-echo $?
-if [ $? -eq 1 ]; then
+status=$?
+if [ $status -eq 1 ]; then
   echo -e "\\e[34m user not exists \\e[0m"
   useradd roboshop &>>/tmp/roboshop.log
   echo $?
